3ae8b619d2736b47d48ce9e30101f55004aec4f3
Tom May <tom@tommay.net>
2011-02-24 04:09:58 +0000

    Fetch the list immediately.

--- public/freedjit.js
+++ public/freedjit.js
@@ -1,16 +1,18 @@
 function freedjit(id, key) {
-    $.get("http://localhost:4567/visit",
+    $.get(
+        "http://localhost:4567/visit",
         { key: key, title: document.title, url: document.URL },
         function(data) {},
         "jsonp");
-    setInterval(
-        function() {
-	    $.get(
-                "http://localhost:4567/list",
-                { key: key },
-                function(data) {
-                    $(id).html(data);
-                },
-                "jsonp");
-        }, 60000);
+    var list = function() {
+        $.get(
+            "http://localhost:4567/list",
+            { key: key },
+            function(data) {
+                $(id).html(data);
+            },
+            "jsonp");
+    };
+    list();
+    setInterval(list, 60000);
 }
